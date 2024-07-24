773cd81ce759d9b7a808731329f4a83853c0abad
LoftLogic <evanlie.737@gmail.com>
2024-07-24 13:59:44 -0700

    Changed testing and research notes

--- testing.py
+++ testing.py
@@ -7,7 +7,7 @@ db_pswd = str(input("What is your db password?: "))
 db_port = int(input("What is the port number? (5432 by default): "))
 DB_PARAMETERS : dict = {"dbname" : db_name, "user" : "postgres", "password" : db_pswd, "host" : "localhost", "port" : db_port}
 
-queries : list[dict] = [{"name" : "Name Here", "SQL" : "SELECT * FROM organization WHERE continent = 'Asia';", 
+queries : list[dict] = [{"name" : "Find Asian Organizations", "SQL" : "SELECT top FROM organization WHERE continent = 'Asia';", 
                           "Expected Output" : "Organization 1"}]
 
 @pytest.fixture(scope='module')
@@ -25,8 +25,7 @@ def test_sql_query(db_connection, query):
         with open(query['expected_output'], 'r') as file:
             expected_output = json.load(file)
 
-        # Convert result to a comparable format (list of dicts)
-        colnames = [desc[0] for desc in cursor.description]
+        colnames = [descrip[0] for descrip in cursor.description]
         result_dicts = [dict(zip(colnames, row)) for row in result]
 
         assert result_dicts == expected_output, f"Query {query['name']} failed"
